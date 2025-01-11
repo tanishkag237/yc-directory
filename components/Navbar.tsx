95564382b787e4f52102f0a1c3551a912ff69e65
tanishkag237 <tgoel029@gmail.com>
2025-01-11 19:03:24 +0530

    before canary

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar =async () =>{
                 <button className='px-1' type='submit'> Logout</button>
             </form>
 
-            <Link href={`/user/${session?.id}`}>
+            <Link href={`/user/${session?.user?.id}`}>
                 <span>{session?.user?.name}</span>
             </Link>
             </>
